742cfa8959c7cb8e90f0e2f49b2bfc315ab0d57b
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-05-19 22:25:18 +0300

    Create Spring Cloud Gateway Project

---
+++ src/test/java/kz/zhelezyaka/spring6gateway/Spring6GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.spring6gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
